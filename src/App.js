ed1bb41ebcaac0d98adc1e447a2a56d06c9344e7
laoye_jnr <idrisaboladeo@gmail.com>
2022-02-25 22:34:46 +0100

    :sparkles: Mint NFT(domain)
    update contract address when contract succesfully deploys

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
+import { ethers } from "ethers";
+import contractABI from "./utils/contractAbi.json";
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
@@ -32,6 +34,63 @@ const App = () => {
       setCurrentAccount(account);
     } else {
       console.log("No authorized account found");
+    }
+  };
+
+  const mintDomain = async () => {
+    if (!domain) {
+      return;
+    }
+
+    if (domain.length < 3) {
+      alert("Domain must be at least 3 characters long");
+      return;
+    }
+    // Calculate price based on length of domain (change this to match your contract)
+    // 3 chars = 0.5 MATIC, 4 chars = 0.3 MATIC, 5 or more = 0.1 MATIC
+    const price =
+      domain.length === 3 ? "0.5" : domain.length === 4 ? "0.3" : "0.1";
+    console.log("Minting domain", domain, "with price", price);
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          contractABI.abi,
+          signer
+        );
+
+        console.log("Going to pop wallet now to pay gas...");
+        let tx = await contract.register(domain, {
+          value: ethers.utils.parseEther(price),
+        });
+        // Wait for the transaction to be mined
+        const receipt = await tx.wait();
+
+        // Check if the transaction was successfully completed
+        if (receipt.status === 1) {
+          console.log(
+            "Domain minted! https://mumbai.polygonscan.com/tx/" + tx.hash
+          );
+
+          // Set the record for the domain
+          tx = contract.setRecord(domain, record);
+          await tx.wait();
+
+          console.log(
+            "Record set! https://mumbai.polygonscan.com/tx/" + tx.hash
+          );
+
+          setRecord("");
+          setDomain("");
+        } else {
+          alert("Transaction failed! Please try again");
+        }
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -96,20 +155,16 @@ const App = () => {
         />
 
         <div className="button-container">
-          <button
-            className="cta-button mint-button"
-            disabled={null}
-            onClick={null}
-          >
+          <button className="cta-button mint-button" onClick={mintDomain}>
             Mint
           </button>
-          <button
+          {/* <button
             className="cta-button mint-button"
             disabled={null}
             onClick={null}
           >
             Set data
-          </button>
+          </button> */}
         </div>
       </div>
     );
